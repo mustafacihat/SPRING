a31b3463131cc7c314da165f352cc02448aa46fa
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2020-10-18 21:41:03 +0200

    update of container

--- Spring-01-SpringContainer/src/main/java/CybertekApp.java
+++ Spring-01-SpringContainer/src/main/java/CybertekApp.java
@@ -1,8 +1,7 @@
 import interfaces.Mentor;
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import services.FullTimeMentor;
+
 
 public class CybertekApp {
 
