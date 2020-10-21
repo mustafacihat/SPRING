5fb978c31ff9ed719900fd9f875a2be065bb3577
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2020-10-21 17:29:46 +0200

    DI-Constructor Injection

--- Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
@@ -3,8 +3,10 @@ package com.cybertek.services;
 import com.cybertek.interfaces.Course;
 
 public class Java implements Course {
+
+    OfficeHours officeHours;
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly Teaching Hours : 20");
+        System.out.println("Weekly Teaching Hours : "+(20 + officeHours.getHours()));
     }
 }
