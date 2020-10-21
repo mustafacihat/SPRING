af309f23dfe0fa93a07d06cc8ddcb7fb332696f7
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2020-10-21 19:33:20 +0200

    DI-Setter Injection

---
+++ Spring-03-BeanScope-Singleton-Prototype/src/main/java/com/cybertek/services/Selenium.java
@@ -0,0 +1,17 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+
+import lombok.Data;
+
+@Data
+public class Selenium implements Course {
+
+
+
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly Teaching Hours : 15");
+    }
+}
