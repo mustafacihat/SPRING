dc411d12ed01eff54877d74b3278a3b6fcdd71a2
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2020-10-24 18:46:56 +0200

    Java annotations - Qualifier

---
+++ Spring-05-JavaAnnotations-Qualifier/src/main/java/com/cybertek/services/Selenium.java
@@ -0,0 +1,27 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Selenium implements Course {
+
+    private ExtraSessions extraSessions;
+
+    public Selenium(@Qualifier("mockInterviewHours") ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
+   /* @Autowired
+    public void setExtraSessions(@Qualifier("mockInterviewHours") ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }*/
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly teaching hours : " + (20 + extraSessions.getHours()));
+    }
+}
