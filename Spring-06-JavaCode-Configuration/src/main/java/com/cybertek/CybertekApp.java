20e7e15426bf6832bc3bbda2921bdc26acbd804e
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2020-10-24 21:02:32 +0200

    Java Code - Configuration

--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/CybertekApp.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/CybertekApp.java
@@ -2,6 +2,7 @@ package com.cybertek;
 
 import com.cybertek.configs.CybertekAppConfig;
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -17,5 +18,10 @@ public class CybertekApp {
         Course courseSelenium = container.getBean("selenium", Course.class);
 
         courseSelenium.getTeachingHourse();
+
+        System.out.println(course.toString());
+
+        ExtraSessions officeHours = container.getBean("extraSessions", ExtraSessions.class);
+        System.out.println(officeHours.getHours());
     }
 }
