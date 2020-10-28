95aef59c6ca17620ec073b23d0926eea3d01e0dc
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2020-10-28 23:24:11 +0100

    SpringBoot-Demo update

--- Spring-07-SpringBoot-Demo/src/main/java/com/cybertek/Spring07SpringBootDemoApplication.java
+++ Spring-07-SpringBoot-Demo/src/main/java/com/cybertek/Spring07SpringBootDemoApplication.java
@@ -1,13 +1,30 @@
 package com.cybertek;
 
+import com.cybertek.interfaces.Course;
+import com.cybertek.services.MockInterviewHours;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Spring07SpringBootDemoApplication {
 
+	@Bean
+	public MockInterviewHours mockInterviewHours(){
+		return new MockInterviewHours();
+	}
 	public static void main(String[] args) {
-		SpringApplication.run(Spring07SpringBootDemoApplication.class, args);
+		ApplicationContext container = SpringApplication.run(Spring07SpringBootDemoApplication.class, args);
+
+		Course javaCourse = container.getBean("java",Course.class);
+
+		System.out.println("javaCourse.getTeachingHours() = " + javaCourse.getTeachingHours());
+
+		Course seleniumCourse = container.getBean("selenium", Course.class);
+		System.out.println("seleniumCourse.getTeachingHours() = " + seleniumCourse.getTeachingHours());
+
+		System.out.println(seleniumCourse.toString());
 	}
 
 }
