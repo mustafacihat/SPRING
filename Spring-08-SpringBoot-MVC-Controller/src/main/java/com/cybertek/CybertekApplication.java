dee95580280d12b96071878048e6ff68e04365fa
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2020-11-14 14:39:17 +0100

    SpringBoot-MVC Controller

--- Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/CybertekApplication.java
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/CybertekApplication.java
@@ -3,11 +3,13 @@ package com.cybertek;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class CybertekApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CybertekApplication.class, args);
-	}
 
+	}
 }
--- Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
@@ -2,10 +2,7 @@ package com.cybertek.controller;
 
 import lombok.Getter;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
@@ -15,9 +12,9 @@ public class HomeController {
         return "home";
     }
 
-    @RequestMapping(method = RequestMethod.GET, value= "/ozzy")
+    @RequestMapping(method = RequestMethod.GET, value= "/java")
     public String getRequestMapping2() {
-        return "home";
+        return "java-features";
     }
 
     @RequestMapping(method = RequestMethod.POST, value= "/ozzy")
@@ -32,6 +29,32 @@ public class HomeController {
 
     @PostMapping("/spring")
     public String postMappingEx(){
+        return "java-features";
+    }
+
+    @GetMapping("/home/{name}")
+    public String pathVariableEx(@PathVariable("name") String name){
+
+        System.out.println("name is: " + name );
+        return "java-features";
+    }
+    @GetMapping("/home/{name}/{email}")
+    public String pathVariableEx2(@PathVariable("name") String name, @PathVariable("email") String email){
+
+        System.out.println("name is: " + name );
+        System.out.println("email is " + email);
+        return "java-features";
+    }
+
+    @GetMapping("home/course")
+    public String requestParamEx(@RequestParam("course") String course){
+        System.out.println("name is : " + course);
+        return "home";
+    }
+
+    @GetMapping(value = "/home/course2")
+    public String requestParam2(@RequestParam(value = "course2", required = false,defaultValue = "Cybertek") String name){
+        System.out.println("name is: " + name);
         return "home";
     }
 
